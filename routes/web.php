d07386124941160adffee53ec436e3b3ad7b9bf6
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-05-07 23:04:25 +0800

    feat!: install Breeze for real this time

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::livewire('/', 'pages::counter');
-Route::livewire('/post/create', 'pages::post.create');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
