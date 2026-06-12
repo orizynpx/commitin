40110e782fc3099ee2197c6afe91c3c7b84d6d16
HamkaArifani <hamka0897@gmail.com>
2026-06-12 17:07:52 +0800

    feat: completing Modify  UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,10 +26,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'student_id' => fake()->unique()->numerify('#############'),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password' => Hash::make('password'),
+            'role' => fake()->randomElement(['sysadmin', 'user']),
+            'is_verified' => fake()->boolean(20),
         ];
     }
 
