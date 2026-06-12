6b0848eaa694e4b21bdd3fde5b459c8d87f7aae4
HamkaArifani <hamka0897@gmail.com>
2026-06-12 17:00:04 +0800

    feat: add title and description to experiences migration file

--- database/migrations/2026_05_30_172012_create_experiences_table.php
+++ database/migrations/2026_05_30_172012_create_experiences_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('experiences', function (Blueprint $table) {
             $table->uuid('experience_id')->primary();
             $table->foreignUuid('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->string('title', 100);
+            $table->text('description');
             $table->timestamps();
         });
     }
