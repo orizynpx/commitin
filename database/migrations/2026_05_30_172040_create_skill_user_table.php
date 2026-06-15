556d71d7fe5f8567097bc3b70dd6e1721c6bdf29
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-15 14:00:59 +0800

    fix: rename pivot tables user_skills to skill_user, vacancy_skills to skill_vacancy

---
+++ database/migrations/2026_05_30_172040_create_skill_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('skill_user', function (Blueprint $table) {
+            $table->foreignUlid('skill_id')->constrained('skills', 'skill_id')->onDelete('cascade');
+            $table->foreignUlid('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->primary(['skill_id', 'user_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('skill_user');
+    }
+};
