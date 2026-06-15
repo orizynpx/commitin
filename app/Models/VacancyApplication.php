f468720eed03fb17a6cc98e3109b0ad520aed26d
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-16 01:03:39 +0800

    feat(models): implement VacancyApplication

--- app/Models/VacancyApplication.php
+++ app/Models/VacancyApplication.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+#[Fillable(['user_id', 'vacancy_id', 'status', 'file_url'])]
 class VacancyApplication extends Model
 {
-    //
+    use HasFactory, HasUlids;
+
+    protected $primaryKey = 'vacancy_application_id';
+
+    /** Relasi ke Pengguna yang melamar */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    /** Relasi ke Lowongan Divisi yang dilamar */
+    public function vacancy(): BelongsTo
+    {
+        return $this->belongsTo(Vacancy::class, 'vacancy_id', 'vacancy_id');
+    }
 }
