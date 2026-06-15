8652d1bf89d7ffce52c16459483a67d21846f198
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-16 01:02:43 +0800

    feat(models): implement EventOrganizer

--- app/Models/EventOrganizer.php
+++ app/Models/EventOrganizer.php
@@ -2,9 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class EventOrganizer extends Model
+class EventOrganizer extends Pivot
 {
-    //
+    protected $table = 'event_organizers';
+
+    protected $fillable = [
+        'event_id',
+        'user_id',
+        'organizer_role',
+    ];
 }
