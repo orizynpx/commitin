09f13ab673f52f982d3719967944db4d2eb64265
HamkaArifani <hamka0897@gmail.com>
2026-06-16 13:36:25 +0800

    feat: remove unused seeder due to refactor, and init ExperienceSeeder

--- database/seeders/ExperienceSeeder.php
+++ database/seeders/ExperienceSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class UserSkillSeeder extends Seeder
+class ExperienceSeeder extends Seeder
 {
     /**
      * Run the database seeds.
