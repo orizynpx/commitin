54c256f71fec1aff2d91fff28de058204f1cc323
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-17 01:45:05 +0800

    refactor: simplify web.php routes in preparation of removing the welcome screen

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
+});
 
 require __DIR__.'/auth.php';
