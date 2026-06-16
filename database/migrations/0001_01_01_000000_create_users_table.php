cc5a9ce2d0b97fd89e457d7a1e5614dba8478446
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-17 02:00:36 +0800

    fix: rename sysadmin role to admin

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['sysadmin', 'student', 'organization']);
+            $table->enum('role', ['admin', 'student', 'organization']);
             $table->rememberToken();
             $table->timestamps();
         });
