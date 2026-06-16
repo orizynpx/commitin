2c351a7dd84dfeab489b5ec29dfe183e1df67126
HamkaArifani <hamka0897@gmail.com>
2026-06-16 14:02:10 +0800

    feat: completing SkillSeeder

--- database/seeders/SkillSeeder.php
+++ database/seeders/SkillSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Skill;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class SkillSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Skill::insert([
+            ['skill_name' => 'Leadership'],
+            ['skill_name' => 'Public Speaking'],
+            ['skill_name' => 'Graphic Design'],
+            ['skill_name' => 'Photography'],
+            ['skill_name' => 'Videography'],
+            ['skill_name' => 'UI/UX Design'],
+            ['skill_name' => 'Copywriting'],
+            ['skill_name' => 'Project Management'],
+        ]);
     }
 }
