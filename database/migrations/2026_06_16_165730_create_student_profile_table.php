bfeb98c91bd775432d104a520796dbb30ed8fc21
HamkaArifani <hamka0897@gmail.com>
2026-06-17 15:06:58 +0800

    feat: finishing student and organization profile migration file

--- database/migrations/2026_06_16_165730_create_student_profile_table.php
+++ database/migrations/2026_06_16_165730_create_student_profile_table.php
@@ -12,7 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('student_profile', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('student_profile_id')->primary();
+            $table->foreignUlid('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->string('student_id', 15)->unique();
+            $table->string('student_id', 15)->unique();
+            $table->string('faculty', 100);
+            $table->string('study_program', 100);
+            $table->year('entry_year')->nullable();
+            $table->text('bio')->nullable();
             $table->timestamps();
         });
     }
--- database/migrations/2026_06_16_165744_create_organization_profile_table.php
+++ database/migrations/2026_06_16_165744_create_organization_profile_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('organization_profile', function (Blueprint $table) {
-            $table->id();
+            $table->ulid('organization_profile_id')->primary();
+            $table->foreignUlid('user_id')->constrained('users', 'user_id')->onDelete('cascade');
+            $table->enum('organization_level', ['study_program', 'faculty', 'university']);
+            $table->text('description')->nullable();
+            $table->enum('verification_status', ['pending', 'verified', 'rejected'])->default('pending');
+            $table->timestamp('verified_at')->nullable();
             $table->timestamps();
         });
     }
