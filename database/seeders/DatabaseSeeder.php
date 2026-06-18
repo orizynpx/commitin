ee530731eadffe8da097d55511e8f498701a65f0
HamkaArifani <hamka0897@gmail.com>
2026-06-18 14:41:53 +0800

    feat: fill databaseseeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+        UserSeeder::class,
+        SkillSeeder::class,
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        StudentProfileSeeder::class,
+        OrganizationProfileSeeder::class,
+
+        ExperienceSeeder::class,
+
+        EventSeeder::class,
+        VacancySeeder::class,
+
+        EventOrganizerSeeder::class,
+        SkillUserSeeder::class,
+        SkillVacancySeeder::class,
+
+        VacancyApplicationSeeder::class,
+    ]);
     }
 }
