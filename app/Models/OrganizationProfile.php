13ee844bf8cf26ec98be3fdeffebab1fea25be3b
HamkaArifani <hamka0897@gmail.com>
2026-06-18 15:09:31 +0800

    feat: config oganization and student profile model

--- app/Models/OrganizationProfile.php
+++ app/Models/OrganizationProfile.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class OrganizationProfile extends Model
 {
     use HasUlids;
+
+    protected $table = 'organization_profile';
+
+     protected $primaryKey = 'organization_profile_id';
+
+    protected $fillable = [
+        'user_id',
+        'organization_level',
+        'description',
+        'verification_status',
+        'verified_at',
+    ];
 }
--- app/Models/StudentProfile.php
+++ app/Models/StudentProfile.php
@@ -7,5 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class StudentProfile extends Model
 {
+
     use HasUlids;
+
+    protected $table = 'student_profile';
+
+    protected $primaryKey = 'student_profile_id';
+
+    protected $fillable = [
+        'user_id',
+        'student_id',
+        'faculty',
+        'study_program',
+        'entry_year',
+        'bio',
+    ];
 }
