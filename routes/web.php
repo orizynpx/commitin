e816e734892c02764dacaa0f36b855b1be58b590
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-19 00:25:16 +0800

    feat: remove landing page (welcome.blade.php) and set routes to redirect the user to the login page

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::get('/', function () {
+    return redirect()->route('dashboard');
+});
+
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
