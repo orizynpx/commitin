53ab85acb1ae48bd0b5b37ef4e908e50749384fa
HamkaArifani <hamka0897@gmail.com>
2026-06-18 14:30:55 +0800

    feat: add SkillVacancy and SkillUser data on seeder

--- database/seeders/SkillUserSeeder.php
+++ database/seeders/SkillUserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Skill;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillUserSeeder extends Seeder
 {
@@ -12,6 +15,32 @@ class SkillUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $bumawati = User::query()->where('email', 'buma@example.com')->first();
+        $andi = User::query()->where('email', 'andi@example.com')->first();
+
+        $leadership = Skill::query()->where('skill_name', 'Leadership')->first();
+        $publicSpeaking = Skill::query()->where('skill_name', 'Public Speaking')->first();
+        $graphicDesign = Skill::query()->where('skill_name', 'Graphic Design')->first();
+
+        DB::table('skill_user')->insert([
+            [
+                'user_id' => $bumawati->user_id,
+                'skill_id' => $leadership->skill_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => $bumawati->user_id,
+                'skill_id' => $publicSpeaking->skill_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'user_id' => $andi->user_id,
+                'skill_id' => $graphicDesign->skill_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
--- database/seeders/SkillVacancySeeder.php
+++ database/seeders/SkillVacancySeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Skill;
+use App\Models\Vacancy;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SkillVacancySeeder extends Seeder
 {
@@ -12,6 +15,32 @@ class SkillVacancySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+         $acara = Vacancy::query()->where('division', 'Acara')->first();
+        $publikasi = Vacancy::query()->where('division', 'Publikasi')->first();
+
+        $leadership = Skill::query()->where('skill_name', 'Leadership')->first();
+        $publicSpeaking = Skill::query()->where('skill_name', 'Public Speaking')->first();
+        $graphicDesign = Skill::query()->where('skill_name', 'Graphic Design')->first();
+
+        DB::table('skill_vacancy')->insert([
+            [
+                'skill_id' => $leadership->skill_id,
+                'vacancy_id' => $acara->vacancy_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'skill_id' => $publicSpeaking->skill_id,
+                'vacancy_id' => $acara->vacancy_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'skill_id' => $graphicDesign->skill_id,
+                'vacancy_id' => $publikasi->vacancy_id,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
