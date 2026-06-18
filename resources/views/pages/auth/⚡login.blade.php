c009593dd5467a5fec56cad6a2fc0a3bd78484c2
Orizyn <71197340+orizynpx@users.noreply.github.com>
2026-06-19 00:33:17 +0800

    feat: add register link to login page

--- resources/views/pages/auth/⚡login.blade.php
+++ resources/views/pages/auth/⚡login.blade.php
@@ -63,6 +63,12 @@ new #[Layout('layouts.guest')] class extends Component
                 </a>
             @endif
 
+            @if (Route::has('register'))
+                <a class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ms-3" href="{{ route('register') }}" wire:navigate>
+                    {{ __('Register') }}
+                </a>
+            @endif
+
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
