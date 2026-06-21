6580d5e6869eed6b0056fb1fbbae4ccef0d00419
Nafis Putra <muhammadnafisputra@gmail.com>
2026-06-22 00:11:37 +0800

    Front 22 Juni v3 Profile mahasiswa

--- resources/views/components/profile/⚡update-password-form.blade.php
+++ resources/views/components/profile/⚡update-password-form.blade.php
@@ -37,11 +37,11 @@ new class extends Component
 
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h2 class="text-2xl font-bold text-on-surface">
             {{ __('Perbarui Kata Sandi') }}
         </h2>
 
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        <p class="mt-2 text-sm text-on-surface-variant">
             {{ __('Pastikan akun Anda menggunakan kata sandi yang panjang dan acak agar tetap aman.') }}
         </p>
     </header>
